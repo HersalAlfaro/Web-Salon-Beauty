610a8321ae9b82087125b839f477e7efac66d6b4
Hersal Alfaro <123428075+HersalAlfaro@users.noreply.github.com>
2024-04-17 14:12:20 -0600

    views

---
+++ Cliente/views/Nosotros.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <!-- basic -->
+    <meta charset="utf-8">
+
+    <!-- mobile metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+    <!-- site metas -->
+    <title>Salon Evolve</title>
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- style css -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Responsive-->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- Scrollbar Custom CSS -->
+    <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+
+</head>
+
+
+<body>
+    <!-- header -->
+    <header>
+        <?php
+        include 'fragments/header.php'
+        ?>
+    </header>
+    <!-- final header -->
+
+    <!-- banner -->
+    <section class="banner_nosotros">
+
+        <div class="carousel-inner-nosotros">
+            <div class="container-fluid">
+                <div class="col-md-12">
+                    <div class="text-bg-nosotros">
+                        <h1>Sobre Nosotros</h1>
+                        <p>
+                            Potenciar tu confianza a través de servicios capilares excepcionales.
+                            Creemos en la importancia de escuchar a nuestros clientes,
+                            comprender sus deseos y necesidades, y trabajar juntos para lograr
+                            resultados sorprendentes. Ya sea que estés buscando un cambio de imagen
+                            completo, un corte de pelo elegante, un tratamiento de color personalizado o un
+                            peinado para una ocasión especial,
+                            estamos aquí para satisfacer todas tus necesidades capilares.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <!-- final banner -->
+
+    <section class="modern-about-us mt-5 mb-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Nuestra Historia</h2>
+                    <p>
+                        En Evolve Salon, nuestra historia es una historia de pasión y dedicación. Comenzamos como un pequeño equipo de apasionados profesionales de la belleza que compartían la visión de crear un espacio único donde las personas pudieran descubrir su mejor versión. Desde el primer día, nos comprometimos a brindar servicios capilares excepcionales que superaran todas las expectativas.
+                        <br>
+                        A lo largo de los años, hemos evolucionado y crecido, pero nuestra dedicación a la excelencia nunca ha vacilado. Nuestro equipo de estilistas y esteticistas, conocido por su habilidad, creatividad y atención al detalle, trabaja incansablemente para realzar la belleza natural de cada cliente. Cada corte de pelo, cada tratamiento de color y cada peinado que creamos es una obra de arte única, diseñada para reflejar la personalidad y el estilo individual de quienes confían en nosotros.
+                    </p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Nuestro Equipo</h2>
+                    <p>
+                        En Evolve Salon, nuestro equipo es el núcleo de nuestro compromiso con la belleza y la confianza. Cada miembro de nuestro talentoso grupo de estilistas y esteticistas aporta su pasión y experiencia a nuestro salón. Están dedicados a brindarte un servicio excepcional y a hacerte sentir lo mejor posible en cada visita.
+                        <br>
+                        Nuestro equipo está formado por profesionales apasionados que siguen de cerca las últimas tendencias de la moda y las técnicas más avanzadas en el mundo de la belleza. Cada uno de ellos está comprometido a escucharte, entender tus deseos y necesidades, y trabajar en colaboración contigo para lograr resultados sorprendentes. La creatividad fluye en sus venas, y están listos para transformar tu imagen con cortes de cabello elegantes, tratamientos de color personalizados y peinados únicos.
+                        <br>
+                        Además de su habilidad técnica, nuestros estilistas y esteticistas se esfuerzan por crear una experiencia relajante y rejuvenecedora.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!--  footer -->
+    <footer id="contacto">
+        <?php
+        include 'fragments/footer.php'
+        ?>
+    </footer>
+
+
+</body>
